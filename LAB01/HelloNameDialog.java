94f78b68a351dfa5c9fded6b57309a5789ac0f2a
minh trung <trung.nm226004@sis.hust.edu.vn>
2024-03-13 21:38:51 +0700

    LAB01

---
+++ LAB01/HelloNameDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class HelloNameDialog {
+    public static void main(String[] args) {
+        String result = JOptionPane.showInputDialog(null, "Please enter your name:");
+        JOptionPane.showMessageDialog(null, "Hello, " + result + "!");
+        System.exit(0);
+    }
+}
